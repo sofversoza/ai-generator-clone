4a59f9a24e8947d9a16533c2ea9d04e9124847cf
sofiaversoza <sofversoza@gmail.com>
2023-06-27 14:40:42 -0700

    client side ui 95% fin

---
+++ client/src/utils/index.js
@@ -0,0 +1,13 @@
+// different functions to use across the whole app
+
+import { surpriseMePrompts } from "../constants"
+
+export function getRandomPrompt(prompt) {
+	const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length)
+	const randomPrompt = surpriseMePrompts[randomIndex]
+
+	// make sure we dont get the same random prompt 2-3x in a row
+	if (randomPrompt === prompt) return getRandomPrompt(prompt)
+
+	return randomPrompt
+}
